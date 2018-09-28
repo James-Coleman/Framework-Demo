5c8d28f53319906e78eda9c7e72374ed989b0da7
James Coleman <mrjameslcoleman@gmail.com>
2018-09-28 07:20:03 +0100

    Started worked on app flow

--- Framework Demo/Flow.swift
+++ Framework Demo/Flow.swift
@@ -10,8 +10,9 @@ import UIKit
 import RxFlow
 
 enum AppStep: Step {
-    case master
-    case detail
+    case home
+    case results(id: Int) // TODO: "id: Int" is a placeholder for now, change it for the actual results selected.
+    case settings
 }
 
 class AppFlow: Flow {
@@ -19,11 +20,11 @@ class AppFlow: Flow {
         return self.rootViewController
     }
     
-    private lazy var rootViewController: UINavigationController = {
-        let navCon = UINavigationController()
-        navCon.navigationBar.barStyle = .blackTranslucent
-        navCon.navigationBar.tintColor = .orange
-        return navCon
+    private lazy var rootViewController: UITabBarController = {
+        let tabBarCon = UITabBarController()
+        let firstTab = UITabBarItem(title: "Results", image: nil, selectedImage: nil)
+        let secondTab = UITabBarItem(title: "News", image: nil, selectedImage: nil)
+        return tabBarCon
     }()
     
     init() {
@@ -34,13 +35,29 @@ class AppFlow: Flow {
         guard let step = step as? AppStep else { return NextFlowItems.none }
         
         switch step {
-        case .master:
-            return enterAtMaster()
-        case .detail:
-            return navigateToDetail()
+        case .home:
+            return startAtHome()
+        case .results(let id):
+            return navigateToResults()
+        case .settings:
+            return showSettings()
         }
     }
     
+    private func startAtHome() -> NextFlowItems {
+        
+    }
+    
+    private func navigateToResults() -> NextFlowItems {
+        
+    }
+    
+    private func showSettings() -> NextFlowItems {
+        
+    }
+    
+    
+    /*
     private func enterAtMaster() -> NextFlowItems {
         let masterVC = MasterVC()
         rootViewController.pushViewController(masterVC, animated: true) // Animated has no effect, probably because this is the root of the navcon, and the navcon knows that it shouldn't be animated.
@@ -52,4 +69,5 @@ class AppFlow: Flow {
         rootViewController.pushViewController(detailVC, animated: true)
         return NextFlowItems.one(flowItem: NextFlowItem(nextPresentable: detailVC, nextStepper: detailVC))
     }
+    */
 }
